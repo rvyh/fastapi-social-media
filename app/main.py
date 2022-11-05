024f64a7a8b5623af3663825ce14420fb1aa7d6d
Ronald Vyhmeister <ronaldvm33@yahoo.com>
2022-11-05 18:54:39 -0300

    Add user resource

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from typing import List
 
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -79,3 +79,26 @@ def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(ge
     db.commit()
     return post_query.first()
 
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password
+    hashed_password = utils.hash_password(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {user_id} was not found")
+
+    return user
